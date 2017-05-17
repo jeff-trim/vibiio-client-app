d21fd1be766e0c2c2a2b161fa6e591f29ee0a2a6
ryantstone <ryan@ryanstone.io>
2017-05-17 14:16:18 -0400

    Proper validation display

--- src/app/password-reset/components/password-reset-form/password-reset-form.component.ts
+++ src/app/password-reset/components/password-reset-form/password-reset-form.component.ts
@@ -8,30 +8,39 @@ import { Credentials } from '../../models/credentials.interface';
 <div>
   <form #form="ngForm"
         novalidate>
-        <input type="email"
+    <input type="email"
            name="email"
            [ngModel]="credentials?.email"
            #email = "ngModel"
            placeholder="Enter Email"
            pattern="^[a-zA-Z0–9\\_\\.\\+\\-]+\\@[a-zA-Z0–9\\-]+\\.[a-zA-Z0–9\\-\\.]+$"
+           (blur)="onBlur($event, email)"
            required>
-    <div *ngIf="email.errors?.pattern && email.touched">
-      	🤔 Email address is invalid
+    <div class="row" id="validation-container">
+      <div [style.display]="(!emailValid && email.touched) ? 'block' : 'none'">
+        🤔 Email address is invalid
+      </div>
+      <div [style.display]="(!emailPresence && email.touched) ? 'block' : 'none'">
+          🤔 Email address is required
+      </div>
     </div>
-
-    <div *ngIf="email.errors?.required && email.touched">
-      	🤔 Email address is required
-    </div>
-    <div class="center-xs">
-      <button type="submit"
-              class="button button-primary"
-              id="reset"
-              (click)="reset(form.value, form.valid)"
-              [disabled]="!email.valid">Reset Password</button>
-    </div>
-    <div class="center-xs">
-      <button class="button button-primary"
-              id="cancel">Cancel</button>
+    <div id="button-container" class="row">
+      <div class="box">
+        <div class="center-xs">
+          <button type="submit"
+                  class="button button-primary"
+                  id="reset"
+                  (click)="reset(form.value, form.valid)"
+                  [disabled]="!email.valid">Reset Password</button>
+        </div>
+      </div>
+      <br />
+      <div class="box">
+        <div class="center-xs">
+          <button class="button button-primary"
+                  id="cancel">Cancel</button>
+        </div>
+       </div>
     </div>
   </form>
 </div>
@@ -39,12 +48,28 @@ import { Credentials } from '../../models/credentials.interface';
 })
 
 export class PasswordResetFormComponent {
-    credentials: Credentials;
+    credentials: Credentials
+    emailValid: Boolean = false
+    emailPresence: Boolean = false
 
     @Output()
     submitPasswordReset: EventEmitter<any> = new EventEmitter<any>();
 
     constructor() {}
+
+    onBlur(event, email){
+        if (email.errors === null) {
+            this.emailValid = true
+        } else if (email.errors.pattern) {
+            this.emailValid = false
+        }
+
+        if (email.errors === null) {
+            this.emailPresence = true
+        } else if (email.errors.required) {
+            this.emailPresence = false
+        }
+    }
 
     reset(value: Credentials, isValid: boolean){
         if(isValid){
