f37456707e48fa64333e79d338e3577c298cee16
Ryan Stone <ryan@ryanstone.io>
2017-07-18 08:05:28 -0400

    update production env vars"

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -4,3 +4,4 @@ export const environment = {
 
 export const API_URL = 'https://vibiio-api-production.herokuapp.com';
 export const OPENTOK_API_KEY = '45807082';
+export const ACTION_CABLE_URL = 'ws://vibiio-api-staging.herokuapp.com/cable'
