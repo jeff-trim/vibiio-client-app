74c87a22904fa444190b6beb365b250d770f4346
Ryan Stone <ryan@ryanstone.io>
2017-07-18 08:40:18 -0400

    update url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true
 };
 
-export const API_URL = 'https://vibiio-api-production.herokuapp.com';
+export const API_URL = 'https://vibiio-api-staging-pr-121.herokuapp.com/';
 export const OPENTOK_API_KEY = '45807082';
 export const ACTION_CABLE_URL = 'ws://vibiio-api-staging.herokuapp.com/cable'
