624245c733521ea2640168fc445a52931d6ad994
sgilliam15 <stevengilliamjr@gmail.com>
2017-08-08 23:10:54 -0700

    -fix styles -update save snapshot params -update snapshpt service -fix typo in appointment interface -debug subscribe

--- src/app/dashboard/services/video-snapshot.service.ts
+++ src/app/dashboard/services/video-snapshot.service.ts
@@ -11,19 +11,22 @@ import 'rxjs/add/observable/throw';
 export class VideoSnapshotService {
   constructor(private http: Http) {}
 
-  saveSnapshot(id: number, snapshot: any) {
-    const url = `${API_URL}/video_chat/snapshot`;
+  saveSnapshot(consumer_id: number, session_id: string, snapshot: any): Observable<any> {
+    const url = `${API_URL}/video_chat/snapshots`;
+    console.log('snapshot', snapshot);
+    console.log(url);
 
     const payload = {
-      video_chat: {
-        session_id: id,
-        snapshot: snapshot
+      video_session: {
+        consumer_id: consumer_id,
+        session_id: session_id,
+        image: `data:image/png;base64,${snapshot}`
       }
     };
 
     return this.http
                .post(url, payload)
-               .map( (response: Response) => response.json() )
+               .map( (response: Response) => response.json(), console.log('post') )
                .catch( (error: any) => Observable.throw(error));
   }
 }
