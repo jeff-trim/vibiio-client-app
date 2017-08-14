18ef7eb41ea2946659bcf520f7c68b11e45a2975
sgilliam15 <stevengilliamjr@gmail.com>
2017-08-14 12:49:38 -0700

    -upadate route to archive service

--- src/app/dashboard/services/video-archive.service.ts
+++ src/app/dashboard/services/video-archive.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { API_URL } from '../../../environments/environment';
 
-const VIDEO_ARCHIVE_API = `${API_URL}/video_chat/archives/?session_id=`;
+const VIDEO_ARCHIVE_API = `${API_URL}/video_chat/archives/`;
 
 @Injectable()
 export class VideoArchiveService {
