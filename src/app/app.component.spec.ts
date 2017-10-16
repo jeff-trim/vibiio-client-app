66db2e60f9fa2f47eda6bf7634df62ee02bddcc0
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2017-10-16 11:54:08 -0400

    Fixes app component specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,47 +1,55 @@
-// import { TestBed, async } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { AppComponent } from './app.component';
-// import { RouterModule, Routes } from '@angular/router';
-// // import { RouterOutletStubComponent } from '../../testing/router-stubs';
-// // import { RouterLinkStubDirective } from '../../testing/router-stubs';
+import { TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterOutletComponent } from '../../testing/router-outlet-stub';
+import { routerStub } from '../../testing/router-stub';
+import { SpinnerComponent } from './easy-spinner/components/spinner.component';
+import { SpinnerService } from './easy-spinner/services/spinner.service';
+import { SpinnerServiceStub } from '../../testing/spinner-service-stub';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+@Component({
+  selector: 'app-spinner',
+  template: `
+  <div>
+  </div>
+  `
+})
+class MockSpinnerComponent { }
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+  TestBed.configureTestingModule({
+    declarations: [
+      AppComponent,
+      RouterOutletComponent,
+      MockSpinnerComponent
+    ],
+    providers: [
+      { provide: Router, useValue: routerStub },
+      { provide: SpinnerService, useClass: SpinnerServiceStub }
+    ]
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+  it(`should have as a title`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    console.log(app);
+    console.log(app.title);
+    expect(app.title).toEqual('Vibiio');
+  }));
+});
 
 
-// describe('AppComponent', () => {
-//     beforeEach(async(() => {
-//         class RouterStub {
-//             navigateByUrl(url: string) { return url; }
-//         }
 
-//       TestBed.configureTestingModule({
-//     imports: [
-//         RouterModule
-        
-//     ],
-//       declarations: [
-//           AppComponent      ],
-//           providers: [
-//               { provide: Router, useClass: RouterStub }
-//           ]
-//     }).compileComponents();
-//   }));
 
-//   it('should create the app', async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app).toBeTruthy();
-//   }));
-
-//   it(`should have as a title`, async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app.title).toEqual('Vibiio Client App');
-//   }));
-
-//   it('should render title in a h1 tag', async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.debugElement.nativeElement;
-//     expect(compiled.querySelector('h1').textContent).
-//       toContain('Vibiio Client App');
-//   }));
-// });
