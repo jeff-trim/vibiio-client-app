21080bdf4405700752a39b4078dc58eac9f58e60
sgilliam15 <stevengilliamjr@gmail.com>
2017-10-18 16:23:56 -0400

    -remove new insurance form from appointment details and consumer profile
    -disable inputs on existing policy detail component
    
    -rename policy edit component

---
+++ src/app/dashboard/components/policy-detail-edit/policy-detail-edit.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input } from '@angular/core';
+import { Form } from '@angular/forms';
+
+// Services
+import { InsurancePolicyService } from '../../services/insurance-policy.service';
+
+// Models
+import { InsurancePolicy } from '../../models/insurance-policy.interface';
+
+@Component({
+    selector: 'vib-policy-detail-edit',
+    templateUrl: 'policy-detail-edit.component.html',
+    styleUrls: ['policy-detail-edit.component.scss']
+})
+
+export class PolicyDetailEditComponent {
+    @Input()
+    policy?: InsurancePolicy;
+
+    constructor(private updateService: InsurancePolicyService) { }
+
+    onSubmit(policy: InsurancePolicy) {
+        const options = {
+            carrier: policy.carrier,
+            policy_number: policy.policy_number,
+            claim_id: policy.claim_id
+        };
+
+        this.updateService
+            .updatePolicy(options, policy.id)
+            .subscribe( (data) => {
+                this.policy = data.insurance_policy;
+            },
+            (error: any) => {
+                console.log( 'error updating note' );
+        });
+    }
+}
