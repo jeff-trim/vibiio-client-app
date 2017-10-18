4d33d5b4e375a4830f5886f8f8359a94252b756f
sgilliam15 <stevengilliamjr@gmail.com>
2017-10-18 13:02:49 -0400

    add validations, display error messages, reset my profile form

--- src/app/dashboard/components/profile-licensure/profile-new-licensure.component.ts
+++ src/app/dashboard/components/profile-licensure/profile-new-licensure.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, Validator } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,13 @@ import { FormGroup, FormControl, Validators, Validator } from '@angular/forms';
     templateUrl: 'profile-new-licensure.component.html'
 })
 
-export class ProfileNewLicensureComponent {
+export class ProfileNewLicensureComponent implements OnInit {
     myLicenseForm: FormGroup;
 
     @Output()
     sumbitLicenseForm = new EventEmitter<any>();
 
-    constructor() {
+    ngOnInit() {
         this.myLicenseForm = new FormGroup({
                 'state': new FormControl('', Validators.required),
                 'licenseNumber': new FormControl('', Validators.required)
