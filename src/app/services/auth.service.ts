06fdc8c0b62d6db25e160d2e0ff1580bc992eb83
sgilliam15 <stevengilliamjr@gmail.com>
2017-10-26 11:15:22 -0400

    -update set token method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
+import { LocalStoreManagerService } from './local-store-manager.service';
 
 @Injectable()
 export class AuthService {
   token: string;
   // be sure to change the name of app-token to your-app-name-token
+  constructor(private storageManager: LocalStoreManagerService) {}
   isLoggedIn() {
     if (this.getToken().length > 0) { return true; }
     return false;
   }
 
   getToken() {
-    const token = localStorage.getItem('app-token') || ''
+    const token = localStorage.getItem('app-token') || '';
     return token;
   }
 
-  setToken(token: string) {
-    localStorage.setItem('app-token', token );
+  setToken(token: string, remember: boolean) {
+    if (remember) {
+      this.storageManager.savePermanentData(token, 'app-token');
+    } else {
+      this.storageManager.saveSyncedSessionData(token, 'app-token');
+    }
   }
 
   logout() {
