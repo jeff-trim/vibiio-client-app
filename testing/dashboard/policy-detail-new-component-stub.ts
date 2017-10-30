1c01aa04f88c36e1818901ce60797ad0eb6bf39c
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2017-10-30 10:04:27 -0400

    Fixes failing tests

---
+++ testing/dashboard/policy-detail-new-component-stub.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'vib-policy-detail-new',
+  template: `
+  <div>
+  </div>
+  `
+})
+
+export class MockPolicyDetailNewComponent {
+}
