5bd82f9e5550461cd20f8b226f6f27aa6fc9d735
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2017-12-28 17:03:46 -0500

    Makes vibiiographers saved state the selected item in profile info dropdown

--- src/app/dashboard/components/profile-information/profile-information.component.ts
+++ src/app/dashboard/components/profile-information/profile-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 // Models
@@ -13,9 +13,13 @@ import { states } from '../../models/states';
     templateUrl: 'profile-information.component.html'
 })
 
-export class ProfileInformationComponent implements OnInit {
+export class ProfileInformationComponent implements OnInit, AfterViewInit {
     myProfileForm: FormGroup;
     states: string[];
+    selectedState: string;
+
+    @ViewChild('stateselect')
+    select: ElementRef;
 
     @Input() onEdit = false;
     @Input() myProfile: MyProfile;
@@ -38,6 +42,15 @@ export class ProfileInformationComponent implements OnInit {
                 'state': new FormControl(this.myProfile.address.state, Validators.required),
                 'zip': new FormControl(this.myProfile.address.zip, Validators.required)
         });
+
+        this.selectedState = this.myProfile.address.state;
+    }
+
+    ngAfterViewInit() {
+        if (this.selectedState) {
+          this.myProfileForm.get('state');
+          this.select.nativeElement.dispatchEvent( new Event( 'change' ) );
+        }
     }
 
     checkErrors(field: AbstractControl): boolean {
