211e6b1d494b3fcb97f2a0598e20e3185130b9ca
Steven R Gilliam Jr <sgilliam@trimagency.com>
2017-12-29 10:02:47 -0500

    Revert "Revert "Fix dropdown dashboard styles""

--- src/app/dashboard/components/profile-information/profile-information.component.ts
+++ src/app/dashboard/components/profile-information/profile-information.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 // Models
 import { MyProfileLicense } from '../../models/my-profile-license.interface';
 import { MyProfileService } from '../../services/my-profile.service';
 import { MyProfile } from '../../models/my-profile.interface';
+import { states } from '../../models/states';
 
 @Component({
     selector: 'vib-profile-information',
@@ -12,13 +13,22 @@ import { MyProfile } from '../../models/my-profile.interface';
     templateUrl: 'profile-information.component.html'
 })
 
-export class ProfileInformationComponent implements OnInit {
+export class ProfileInformationComponent implements OnInit, AfterViewInit {
     myProfileForm: FormGroup;
+    states: string[];
+    selectedState: string;
+
+    @ViewChild('stateselect')
+    select: ElementRef;
 
     @Input() onEdit = false;
     @Input() myProfile: MyProfile;
 
     updateProfile: EventEmitter<FormGroup> = new EventEmitter;
+
+    constructor() {
+        this.states = states;
+    }
 
     ngOnInit() {
         this.myProfileForm = new FormGroup({
@@ -32,6 +42,15 @@ export class ProfileInformationComponent implements OnInit {
                 'state': new FormControl(this.myProfile.address.state, Validators.required),
                 'zip': new FormControl(this.myProfile.address.zip, Validators.required)
         });
+
+        this.selectedState = this.myProfile.address.state;
+    }
+
+    ngAfterViewInit() {
+        if (this.selectedState) {
+          this.myProfileForm.get('state');
+          this.select.nativeElement.dispatchEvent( new Event( 'change' ) );
+        }
     }
 
     checkErrors(field: AbstractControl): boolean {
