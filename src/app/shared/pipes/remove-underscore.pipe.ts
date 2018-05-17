f88448ae645c7287333f654eeb5933a7042b2029
sgilliam15 <stevengilliamjr@gmail.com>
2018-05-17 11:16:14 -0400

    linting and styles qa

--- src/app/shared/pipes/remove-underscore.pipe.ts
+++ src/app/shared/pipes/remove-underscore.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'removeUnderscore'})
 export class RemoveUnderscorePipe implements PipeTransform {
   transform(value: string): string {
-    let newValue = value.replace(/\_/g, ' ');
+    const newValue = value.replace(/\_/g, ' ');
     return `${newValue}`;
   }
 }
