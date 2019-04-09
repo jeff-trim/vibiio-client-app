a1d4e093c56757564291dfad2352f2ee93351621
sgilliam15 <stevengilliamjr@gmail.com>
2019-04-09 15:56:43 -0400

    updates consumer status component  and service to handle nil results

--- src/app/dashboard/services/all-consumers.service.ts
+++ src/app/dashboard/services/all-consumers.service.ts
@@ -1,52 +1,50 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { API_URL } from '../../../environments/environment';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { ConsumerProfile } from '../models/consumer-profile.interface';
-import { logging } from 'protractor';
-import { all } from 'q';
 
-const   ALL_CONSUMERS_API = `${API_URL}/all_consumers/`;
+const ALL_CONSUMERS_API = `${API_URL}/all_consumers/`;
 
 @Injectable()
 export class AllConsumersService {
-    constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
-    index(query?: string): Observable<ConsumerProfile[]> {
-      let url = ALL_CONSUMERS_API;
-      if (query) {
-        url = url.concat(`?term=${query}`);
-      }
-      return this.http
-        .get(url)
-        .map((response: any) => response.json())
-        .catch(error => {
-          console.error('An error occurred:', error.error.json());
-          return error.error.json();
-        });
-      }
-
-    byStatus(status?: string, query?: string): Observable<ConsumerProfile[]> {
-      return this.http
-        .get(this.constructUrl(status, query))
-        .map((response: any) => response.json())
-        .catch(error => {
-          console.error('An error occurred:', error.error.json());
-          return error.error.json();
-        });
+  index(query?: string): Observable<ConsumerProfile[]> {
+    let url = ALL_CONSUMERS_API;
+    if (query) {
+      url = url.concat(`?term=${query}`);
     }
+    return this.http
+      .get(url)
+      .map((response: any) => response.json())
+      .catch(error => {
+        console.error('An error occurred:', error.error.json());
+        return error.error.json();
+      });
+  }
 
-    private constructUrl(status?: string, query?: string) {
-      const url = ALL_CONSUMERS_API;
+  byStatus(status?: string, query?: string): Observable<ConsumerProfile[]> {
+    return this.http
+      .get(this.constructUrl(status, query))
+      .map((response: any) => (response.json()))
+      .catch(error => {
+        console.error('An error occurred:', error.error.json());
+        return error.error.json();
+      });
+  }
 
-      if (status && query) {
-        return url.concat(`?status=${status}&term=${query}`);
-      } else if (status) {
-        return url.concat(`?status=${status}`);
-      } else if (query) {
-        return url.concat(`?term=${query}`);
-      }
+  private constructUrl(status?: string, query?: string) {
+    const url = ALL_CONSUMERS_API;
+
+    if (status && query) {
+      return url.concat(`?status=${status}&term=${query}`);
+    } else if (status) {
+      return url.concat(`?status=${status}`);
+    } else if (query) {
+      return url.concat(`?term=${query}`);
     }
+  }
 }
